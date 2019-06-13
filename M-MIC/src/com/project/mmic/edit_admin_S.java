1f0e721275ca81d1fc2ab6cecb653fd22abac168
itsmigo1 <39785786+itsmigo1@users.noreply.github.com>
2019-06-13 13:39:18 +0900

    Update edit_admin_S.java

--- M-MIC/src/com/project/mmic/edit_admin_S.java
+++ M-MIC/src/com/project/mmic/edit_admin_S.java
@@ -19,9 +19,7 @@ public class edit_admin_S extends HttpServlet {
         super();
     }
     
-	private String url_mysql = "jdbc:mysql://106.10.33.249:3306/mmic_db?characterEncoding=utf8&useSSL=false&serverTimezone=Asia/Seoul";
-	private String id_mysql = "mmic";
-	private String pw_mysql = "mmic!@21";
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
